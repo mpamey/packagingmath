bb0728a24227bb59bc546f76192a03ac4c96dc62
maxim de mey <pnl0j92b@Emea.Royalahold.net>
2021-10-08 14:41:58 +0200

    package gemaakt

---
+++ myMath/__init__.py
@@ -0,0 +1,5 @@
+from mymath.advanced.sqrt import squareroot
+from mymath.add import add
+from mymath.divide import division
+from mymath.multiply import multiply
+from mymath.subtract import subtract
