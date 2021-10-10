8cc1b50612e67a044655de01365fbdb29255606c
maxim de mey <pnl0j92b@Emea.Royalahold.net>
2021-10-10 20:04:18 +0200

    more tests to increase coverage

--- make-egg/mymath/test_math.py
+++ make-egg/mymath/test_math.py
@@ -8,3 +8,12 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == mymath.subtract(4, 2)
+
+    def test_squareroot(self):
+        assert 4 == mymath.squareroot(16)
+
+    def test_division(self):
+        assert 4 == mymath.division(8, 2)
+    
+    def test_multiplication(self):
+        assert 4 == mymath.multiply(4, 1)
