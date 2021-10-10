81965812adf8931877c5841c37c2d505305cd1cf
maxim de mey <pnl0j92b@Emea.Royalahold.net>
2021-10-10 19:43:16 +0200

    test linting setup.py

--- make-egg/setup.py
+++ make-egg/setup.py
@@ -1,6 +1,2 @@
 from setuptools import setup, find_packages
-setup(
-    name = "mymath",
-    version = "0.1",
-    packages = find_packages()
-    )
+setup(name = "mymath", version = "0.1", packages = find_packages())
