8d269603f1fb324462119e6c9b3e92ca82c34a43
maxim de mey <pnl0j92b@Emea.Royalahold.net>
2021-10-10 20:00:21 +0200

    if name = main in init

--- make-egg/mymath/__init__.py
+++ make-egg/mymath/__init__.py
@@ -6,7 +6,7 @@ from mymath.subtract import subtract
 
 if __name__ == '__main__':
     print(squareroot(4))
-    print(add(2,2))
-    print(division(2,2))
-    print(multiply(2,2))
-    print(subtract(2,2))
+    print(add(2, 2))
+    print(division(2, 2))
+    print(multiply(2, 2))
+    print(subtract(2, 2))
